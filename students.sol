0b6bd351ab9a97bef4134177d9502d638ef129f2
superman <goodnesskolapo@gmail.com>
2023-02-03 17:38:11 +0100

    done

--- students.sol
+++ students.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.1;
 
-contract Student {    
+contract Students {    
     address private admin_adress;
 
-    struct student{
+    struct Student{
         string name;  
         uint8 age;
         string gender; 
     }
+
+    Student student;
+
+    string details;
  
     constructor() {                 
            admin_adress = msg.sender;
@@ -28,5 +32,27 @@ contract Student {
 
         require(keccak256(abi.encode(_name)) == keccak256(abi.encodePacked(student.name)) , "student not found");
 
+    }
+
+    function getallstudents() public view returns(string memory student){
+
+        return student;
+
+    } 
+
+    function deletestudent(string memory _name) public returns(string memory _gender , uint8 _age){
+
+        _name = student.name;
+
+        // details = 
+        // _name = student.name ,
+        // _gender = student.gender ,
+        // _age = student.age;
+        require(msg.sender == admin_adress , "not authorised");
+        require(keccak256(abi.encode(_name)) == keccak256(abi.encodePacked(student.name)) , "student not found");
+        delete student.name;
+        delete student.gender;
+        delete student.age;
+        
     }
 }
