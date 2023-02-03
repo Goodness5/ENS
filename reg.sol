64a80940b7732ad3544e2fe80690cb4293ee76c0
superman <goodnesskolapo@gmail.com>
2023-02-03 01:24:32 +0100

    Initial commit

---
+++ reg.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.1;
+
+contract Name {
+    
+    address  aaddress;
+    string name;
+
+    function register(string memory _name ) public  {
+        
+    }
+}
