464fd5e264e331e809c6e0bc891b4b052cc03321
superman <goodnesskolapo@gmail.com>
2023-02-06 17:27:02 +0100

    assessment

--- assessment.sol
+++ assessment.sol
@@ -1,2 +1,9 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.1;
+
+
+
+/**
+Write a   contract that deposit fund into a contract. And also keep track of funds transferred into the contract. 
+Add a function to the balance of address that have deposited into the contract.(Submit Github gist)
+ */
